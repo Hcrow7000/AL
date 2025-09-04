a091a16975467f8475b4c42fe44157c51187d053
Hcrow7000 <chanminblue@gmail.com>
2025-09-04 20:15:40 +0900

    2025-09-04 (AL day 17)

--- al/Program.cpp
+++ al/Program.cpp
@@ -8,17 +8,7 @@ using namespace std;
 
 int main()
 {
-#pragma region 쉘 정렬
-	// 리스트를 일정한 간격에 따라 나누고,
-	// 각 부분 리스트를 삽입 정렬을 통해
-	// 정렬하는 방법입니다.
-	
-	// 1. 초기 시작 간격을 설정합니다.
-
-	// 2. 가격단위로 그룹을 묶어서 
-	// 리스트를 나눕니다.
-
-	// 3. 
+#pragma region 
 
 #pragma endregion
 
